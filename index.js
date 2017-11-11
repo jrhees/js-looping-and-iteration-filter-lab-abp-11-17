d76ccf501757b4f8e168db5dfdf56a4e45c4d9c1
Jennifer Rhees <redswinglinestapler23@gmail.com>
2017-11-11 03:05:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
 function findMatching(drivers, driver) {
-  return drivers.filter(function (name) { 
-    return driver === name;
-    
+  return drivers.filter(function (name) {
+    return driver.toLowerCase() === name.toLowerCase();
+
   });
 }
